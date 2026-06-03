b36962d88fae9f8acb76a3681900f8f580e6029f
Carla Guinea <carla.guinea@Carlas-MacBook-Air.local>
2026-06-03 11:45:23 +0200

    helper for trying url

--- download_basis.hpp
+++ download_basis.hpp
@@ -7,6 +7,6 @@
 std::string download_url(const std::string& url);
 std::string download_url(const std::string& url, Downloading_Stats& stats);
 std::string retries_download_url(const std::string& url,Downloading_Stats& stats,int max_retries = 3,int retry_delay_ms = 500);
-
-
+//optimization part: function that measures if it's worth trying the download: with most common cases of mistakes in names
+bool is_valid_url(const std::string& url);
 #endif
